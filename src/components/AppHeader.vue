be3f31cd61e1255d1b5dfacecdd795a9f50b15ca
RiccardoNestola <riccardinux@gmail.com>
2023-01-12 00:37:21 +0100

    add counter

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,17 +1,27 @@
 
 <script>
+import { store } from "../store"
+
 export default {
 
-
+    data() {
+        return {
+            store,
+        }
+    }
 
 }
+
+
+
 </script>
 
 <template>
     <header>
-        <nav class="navbar bg-body-tertiary">
+        <nav class="navbar navbar-dark bg-dark">
             <div class="container-fluid">
                 <span class="navbar-brand mb-0 h1">YU GI OH</span>
+                <p class="navbar-brand mb-0 h1 text-danger">Numbers: {{ store.CharactersList.length }}</p>
             </div>
         </nav>
     </header>
